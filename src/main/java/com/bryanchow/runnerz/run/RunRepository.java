d8a25008a207de33c698986c14f282ac85346374
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2024-12-03 14:53:59 +0800

    Add RunController and RunRepository for Run management
    - Create RunController to handle Run-related HTTP requests
      - Implement endpoint methods for retrieving Run objects
      - @RestController handles HTTP requests and returns the response as JSON or XML data.
      - @RequestMapping maps web requests to specific handler classes or methods.
      - Inject RunRepository as a dependency for data access
    
    - Create RunRepository to manage Run objects in memory
      - Implement findAll() and findById() methods for data retrieval
      - Use @PostConstruct to ensure the init() method is called after the repository is constructed
      - Add @Repository annotation for Spring component management

---
+++ src/main/java/com/bryanchow/runnerz/run/RunRepository.java
@@ -0,0 +1,35 @@
+package com.bryanchow.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id().equals(id))
+                .findFirst();
+    }
+
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plusMinutes(30), 5, Location.OUTDOOR));
+        runs.add(new Run(2, "Second Run", LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(1).plusMinutes(45), 10, Location.INDOOR));
+
+    }
+
+
+}
