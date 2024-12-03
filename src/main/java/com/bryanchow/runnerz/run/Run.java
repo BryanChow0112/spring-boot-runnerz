969e9069dddf007c404e166e48ed5cca2dbe8bac
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2024-12-03 16:24:09 +0800

    Add error handling and validation for Run operations
    - Introduce RunNotFoundException for handling non-existent runs
    
    - Integrate Spring Boot validation dependency
     - Add spring-boot-starter-validation to project dependencies
    
    - Enhance error handling in RunController and RunRepository
     - Implement validation checks for Run object creation and updates

--- src/main/java/com/bryanchow/runnerz/run/Run.java
+++ src/main/java/com/bryanchow/runnerz/run/Run.java
@@ -1,14 +1,27 @@
 package com.bryanchow.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer kilometers,
         Location location
 ) {
 
+    public Run {
+        // We can perform error validation here or using the @Valid annotation
+        // in the controller which is from the jakarta.validation.constraints package
+        // which needs to be added to the pom.xml file as a dependency
+        if (!completedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("Run must be completed after it has started");
+        }
+    }
 }
--- src/main/java/com/bryanchow/runnerz/run/RunController.java
+++ src/main/java/com/bryanchow/runnerz/run/RunController.java
@@ -1,8 +1,8 @@
 package com.bryanchow.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,20 +27,20 @@ public class RunController {
 
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+            throw new RunNotFoundException();
         }
         return run.get();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
