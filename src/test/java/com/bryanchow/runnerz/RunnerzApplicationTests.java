f6ca3c2b9bbf6792fa925bb5b3081ccd60d829ad
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2024-12-03 11:23:51 +0800

    Initial commit for Spring Boot project
    - Set up a new Spring Boot project using Spring Initializr
    - Added dependencies for Spring Web and Spring Boot DevTools
    - Configured basic project structure and initial application properties

---
+++ src/test/java/com/bryanchow/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanchow.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
