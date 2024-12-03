5de2e0a5190e0d762e68333b0a712ab7aa23a2d5
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2024-12-03 15:55:19 +0800

    Add CRUD operations to RunController and RunRepository
    - Enhance RunController with full CRUD endpoint support
     - Implement @PostMapping to create new Run objects
     - Add @PutMapping for updating existing Run objects
     - Implement @DeleteMapping to remove Run objects by ID
     - Use appropriate HTTP status codes for each operation
       - CREATED (201) for new object creation
       - NO_CONTENT (204) for update and delete operations
    
    - Extend RunRepository with corresponding CRUD methods

--- src/main/java/com/bryanchow/runnerz/run/RunController.java
+++ src/main/java/com/bryanchow/runnerz/run/RunController.java
@@ -1,10 +1,7 @@
 package com.bryanchow.runnerz.run;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -30,12 +27,26 @@ public class RunController {
 
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
         }
         return run.get();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
 
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
 }
