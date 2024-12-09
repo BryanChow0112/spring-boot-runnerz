b9ba27ffff699fcca7fb4e7c39381f0677514e39
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2024-12-09 15:46:10 +0800

    Configure H2 database and JDBC repository
    - Added H2 database and JDBC dependencies to pom.xml
    - Utilise Spring Boot's auto-configuration for database setup
    - Created schema.sql in the resources folder to define the database schema
    - Created data.sql in the resources folder to populate the database with initial data
    - Replaced ArrayList with JDBCClient in RunRepository for data management

--- src/main/java/com/bryanchow/runnerz/run/RunRepository.java
+++ src/main/java/com/bryanchow/runnerz/run/RunRepository.java
@@ -1,6 +1,9 @@
 package com.bryanchow.runnerz.run;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,39 +14,11 @@ import java.util.Optional;
 @Repository
 public class RunRepository {
 
-    private List<Run> runs = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
 
-    List<Run> findAll() {
-        return runs;
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
-
-    Optional<Run> findById(Integer id) {
-        return runs.stream()
-                .filter(run -> run.id().equals(id))
-                .findFirst();
-    }
-
-    void create(Run run) {
-        runs.add(run);
-    }
-
-    void update(Run run, Integer id) {
-        Optional<Run> existingRun = findById(id);
-        if (existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
-    }
-
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plusMinutes(30), 5, Location.OUTDOOR));
-        runs.add(new Run(2, "Second Run", LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(1).plusMinutes(45), 10, Location.INDOOR));
-
-    }
-
 
 }
